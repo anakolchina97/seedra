9063d03f6183714531f2ad404cb1913d7690881a
Annie <anniehartmann97@gmail.com>
2023-02-18 17:34:38 +0300

    Add styled-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,43 +7,27 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { createGlobalStyle } from "styled-components"
 
-import Header from "./header"
-import "./layout.css"
+const theme = {
+  colors: {
+    white: "#ffffff",
+    black: "#121720",
+  },
+  breakpoints: {},
+}
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: ${props => props.theme.colors.white};
+  }
+`
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <GlobalStyle theme={theme} />
+      {children}
     </>
   )
 }
